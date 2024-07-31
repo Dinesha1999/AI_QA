003fb3f23b5ae0d0cd63e86354b9b7ab179329a9
lakshithakr <lakshithakumuduranga102@gmail.com>
2024-07-31 20:22:51 +0530

    'setup'

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='AIQA',
+version='0.0.1',
+author='Dinesha',
+author_email='dineshaudayangi99@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
