0bc603ab75ad7b6072f2ffb34cee7ac5e30ca6af
Dinesha1999 <dineshaudayangi99@gmail.com>
2024-09-14 05:35:01 -0700

    'complete'

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st  
 
 from src.utills import extract_text_pdf,extract_text_docx,extract,convert_chunks, text_embedding, generate_answer
-
+from bs4 import BeautifulSoup
 
 def main():  
     html_temp = """  
@@ -12,7 +12,12 @@ def main():
     st.markdown(html_temp, unsafe_allow_html=True) 
     data='data.pdf'
     #st.write(dataaaaa)
-    text_data=extract(data)
+    with open('data_convert.html','r') as file:
+        html_content=file.read()
+    soup=BeautifulSoup(html_content,'html.parser')
+    soup_str=str(soup)
+    text_data=soup_str
+    #text_data=extract(data)
     chunks=convert_chunks(text_data)
     vector_db=text_embedding(chunks)
     #st.write(vector_db)
