4bb99e33c86aee4464b1cb4c8df9384fe4b2728d
lakshithakr <lakshithakumuduranga102@gmail.com>
2024-09-16 08:50:19 +0530

    three prompts

--- src/utills.py
+++ src/utills.py
@@ -11,7 +11,7 @@ from langchain.chains.question_answering import load_qa_chain
 load_dotenv(find_dotenv())
 api_key=os.environ.get("OPEN_API_KEY")
 embedding=OpenAIEmbeddings(openai_api_key=api_key)
-llm = OpenAI(api_key=api_key)
+llm = OpenAI(api_key=api_key,max_tokens=512)
 # extract text from PDF document
 def extract_text_pdf(pdf):
     text = ""
@@ -46,8 +46,8 @@ def extract(doc):
 def convert_chunks(text_data):
     text_splitter=CharacterTextSplitter(
     separator="\n",
-    chunk_size=1300,
-    chunk_overlap=300,
+    chunk_size=3000,
+    chunk_overlap=100,
     length_function=len,
     )
     chunks=text_splitter.split_text(text_data)
@@ -71,12 +71,30 @@ def text_embedding(chunks):
 
 def generate_answer(vector_db,query):
     embedding_vector = embedding.embed_query(query)
-    docs = vector_db.similarity_search_by_vector(embedding_vector)
+    docs = vector_db.similarity_search_by_vector(embedding_vector,k=3)
+    # print(len(docs))
+    # print("---------------------------------------------\n")
+    # print(docs[0])
+    # print("---------------------------------------------\n")
+    # print(docs[1])
 
     chain=load_qa_chain(llm=llm,chain_type='stuff')
-    response= chain.run(input_documents=docs,question=query)
+    response1= chain.run(input_documents=[docs[0]],question=query)
+    response2= chain.run(input_documents=[docs[1]],question=query)
+    response3= chain.run(input_documents=[docs[2]],question=query)
+    template = """
+    Given the following three answers to the question "{question}":
+    1. {response1}
+    2. {response2}
+    3. {response3}
 
-    return response
+    Analyze the responses and provide the best final answer.The objective of this method is find the best answer, Hence ignore invalid answers.
+    Final answer:
+    """
+    prompt = template.format(question=query, response1=response1, response2=response2, response3=response3)
+    final_answer = chain.run(input_documents=[], question=prompt)
+
+    return final_answer
 
 
 
